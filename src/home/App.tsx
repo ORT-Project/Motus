65f9a94268d94293abcc109ff427dc00636c0b4f
BluedyAishela_ <94249947+BluedyRimuru@users.noreply.github.com>
2024-01-30 17:24:44 +0100

    #4 Changement du titre et ajustements

--- src/home/App.tsx
+++ src/home/App.tsx
@@ -45,6 +45,23 @@ export default function App () {
                         <p>X</p>
                     </div>
                 </div>
+                <div className="content-accueil">
+                    <div className="motus-border motus-warning">
+                        <p>A</p>
+                    </div>
+                    <div className="motus-border motus-danger">
+                        <p>Z</p>
+                    </div>
+                    <div className="motus-border motus-success">
+                        <p>E</p>
+                    </div>
+                    <div className="motus-border motus-success">
+                        <p>R</p>
+                    </div>
+                    <div className="motus-border motus-success">
+                        <p>X</p>
+                    </div>
+                </div>
             </div>
         </div>
   )

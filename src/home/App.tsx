176fa0adbaaa226c65c2ce99d9aedd1da0d1aef2
BluedyAishela_ <94249947+BluedyRimuru@users.noreply.github.com>
2024-01-30 16:55:20 +0100

    #4 Continuité de découverte de ReactJS + Alignement d'éléments pour le motus

--- src/home/App.tsx
+++ src/home/App.tsx
@@ -7,24 +7,44 @@ import './App.css'
 export default function App () {
   return (
         <div className="section-accueil">
-            <div className="content-accueil">
-                <p>Test</p>
-                <div className="motus-border motus-warning">
-                    <p>M</p>
+            <h1>Jeu du motus</h1>
+            <p>Vous devez trouver un mot de X caractères en X tentatives.</p>
+            <div className="content-history">
+                <input value="test"></input>
+                <div className="content-accueil">
+                    <div className="motus-border motus-warning">
+                        <p>A</p>
+                    </div>
+                    <div className="motus-border motus-danger">
+                        <p>Z</p>
+                    </div>
+                    <div className="motus-border motus-success">
+                        <p>E</p>
+                    </div>
+                    <div className="motus-border motus-success">
+                        <p>R</p>
+                    </div>
+                    <div className="motus-border motus-success">
+                        <p>X</p>
+                    </div>
                 </div>
-                <div className="motus-border motus-danger">
-                    <p>O</p>
+                <div className="content-accueil">
+                    <div className="motus-border motus-warning">
+                        <p>A</p>
+                    </div>
+                    <div className="motus-border motus-danger">
+                        <p>Z</p>
+                    </div>
+                    <div className="motus-border motus-success">
+                        <p>E</p>
+                    </div>
+                    <div className="motus-border motus-success">
+                        <p>R</p>
+                    </div>
+                    <div className="motus-border motus-success">
+                        <p>X</p>
+                    </div>
                 </div>
-                <div className="motus-border motus-success">
-                    <p>T</p>
-                </div>
-                <div className="motus-border motus-success">
-                    <p>U</p>
-                </div>
-                <div className="motus-border motus-success">
-                    <p>S</p>
-                </div>
-
             </div>
         </div>
   )

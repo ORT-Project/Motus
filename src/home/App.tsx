892af19029886ec47491848ba1299ba72ae74607
Neleoko <adda.ephraim@gmail.com>
2024-01-30 15:09:44 +0100

    #16 Ajout du routage

--- src/home/App.tsx
+++ src/home/App.tsx
@@ -4,31 +4,28 @@ import './App.css'
 
 // import ButtonTest from '../components/ButtonTest'
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-function App () {
+export default function App () {
   return (
-      <div className="section-accueil">
-          <div className="content-accueil">
-              <p>Test</p>
-              <div className="motus-border motus-warning">
-                  <p>M</p>
-              </div>
-              <div className="motus-border motus-danger">
-                  <p>O</p>
-              </div>
-              <div className="motus-border motus-success">
-                  <p>T</p>
-              </div>
-              <div className="motus-border motus-success">
-                  <p>U</p>
-              </div>
-              <div className="motus-border motus-success">
-                  <p>S</p>
-              </div>
+        <div className="section-accueil">
+            <div className="content-accueil">
+                <p>Test</p>
+                <div className="motus-border motus-warning">
+                    <p>M</p>
+                </div>
+                <div className="motus-border motus-danger">
+                    <p>O</p>
+                </div>
+                <div className="motus-border motus-success">
+                    <p>T</p>
+                </div>
+                <div className="motus-border motus-success">
+                    <p>U</p>
+                </div>
+                <div className="motus-border motus-success">
+                    <p>S</p>
+                </div>
 
-          </div>
-      </div>
+            </div>
+        </div>
   )
 }
-
-export default App

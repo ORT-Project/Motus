a61e1ff51dc607fd49111e15c5431330feaa8f7e
BluedyAishela_ <94249947+BluedyRimuru@users.noreply.github.com>
2024-01-30 14:47:21 +0100

    #4 Page d'accueil + Découverte de ReactJS

--- src/home/App.tsx
+++ src/home/App.tsx
@@ -1,31 +1,32 @@
 import React from 'react'
-import logo from '../resources/logo.svg'
+// import logo from '../resources/logo.svg'
 import './App.css'
 
-import ButtonTest from '../components/ButtonTest'
+// import ButtonTest from '../components/ButtonTest'
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 function App () {
   return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <div>
-            <p>Développeurs</p>
+      <div className="section-accueil">
+          <div className="content-accueil">
+              <p>Test</p>
+              <div className="motus-border motus-warning">
+                  <p>M</p>
+              </div>
+              <div className="motus-border motus-danger">
+                  <p>O</p>
+              </div>
+              <div className="motus-border motus-success">
+                  <p>T</p>
+              </div>
+              <div className="motus-border motus-success">
+                  <p>U</p>
+              </div>
+              <div className="motus-border motus-success">
+                  <p>S</p>
+              </div>
+
           </div>
-          <ButtonTest />
-        </header>
       </div>
   )
 }

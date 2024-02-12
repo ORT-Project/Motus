036cd4d2d0dae067998eebc53bea6b7ec510a08b
Ephraim <98337057+Neleoko@users.noreply.github.com>
2024-02-12 10:49:31 +0100

    7 limiter la taille de linput par rapport au mot du dictionnaire sélectionné (#20)
    * #7 Ajout de la limite de caractere dans l'input par rapport au mot
    
    * #7 Modification de la constance en fonction (à vérifier prochain cours)
    
    * #7 modification de la constance de la fonction (final)
    
    ---------
    
    Co-authored-by: BluedyAishela_ <94249947+BluedyRimuru@users.noreply.github.com>

--- src/components/DictionaryRuleProcessor.tsx
+++ src/components/DictionaryRuleProcessor.tsx
@@ -1,7 +1,9 @@
-import React from 'react' // , { useState }
+import React, { useEffect } from 'react' // , { useState }
 import words from 'an-array-of-french-words'
 
-export default function DictionaryRuleProcessor () {
+export type DictionaryRuleProcessorPros = { answer: string, setAnswer: (answer: string) => void }
+
+export default function DictionaryRuleProcessor ({ setAnswer, answer }: DictionaryRuleProcessorPros) {
 	// state (état, données)
 	// const [minLetters, setMinLetters] = useState(5)
 	// const [maxLetters, setMaxLetters] = useState(10)
@@ -11,12 +13,18 @@ export default function DictionaryRuleProcessor () {
 	// comportements
 	const wordList: string[] = (words as any[]).filter((word: string) => (
 		/^[a-z]+$/.test(word) &&
-		word.length > minLetters &&
-		word.length <= maxLetters)
+        word.length > minLetters &&
+        word.length <= maxLetters)
 	) as string[]
 
-	const randInt = Math.floor(Math.random() * wordList.length)
-	const answer = wordList[randInt].toUpperCase()
+	const generateWord = () => {
+		const randInt = Math.floor(Math.random() * wordList.length)
+		setAnswer(wordList[randInt].toUpperCase())
+	}
+
+	useEffect(() => {
+		generateWord()
+	}, [])
 
 	// affichage (render)
 	return (

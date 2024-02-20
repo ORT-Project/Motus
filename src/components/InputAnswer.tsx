2a425f9beb8ce6aacb80e61272de4da04057de6d
Ephraim Adda <adda.ephraim@gmail.com>
2024-02-20 15:02:33 +0100

    #8 Ajout de couleurs par rapport aux placements des lettres bien placé ou pas + ajouts de quelques verifications d'erreurs + commentaires

--- src/components/InputAnswer.tsx
+++ src/components/InputAnswer.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import words from 'an-array-of-french-words'
 
 export type WordHistoryProps = {
 	answer: string
@@ -14,6 +15,7 @@ export default function InputAnswer ({
 	inputValue,
 	setInputValue
 }: WordHistoryProps) {
+	const [textErrorInput, setTextErrorInput] = useState<string>('')
 	const nbInputStringLeft = answer.length - inputValue.length
 
 	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -23,6 +25,20 @@ export default function InputAnswer ({
 	function verifyInput (answer: string) {
 		if (inputValue.length !== answer.length) return false // si pas la bonne longueur
 		if (!/^[a-zA-Z]*$/.test(inputValue)) return false // si pas que des lettres
+		if (!errorInput(inputValue.toLowerCase())) return false // si pas dans le dictionnaire
+		if (historyInput.includes(inputValue.toUpperCase())) { // si déjà utilisé
+			setTextErrorInput('Le mot ' + inputValue + ' a déjà été utilisé. Choisissez un autre mot.')
+			return false
+		}
+		return true
+	}
+
+	function errorInput (InputValue: string) {
+		if (!(words as any[]).includes(inputValue.toLowerCase())) {
+			setTextErrorInput('Le mot ' + inputValue + ' n\'existe pas dans le dictionnaire. Veuillez réessayer.') // erreur
+			return false
+		}
+		setTextErrorInput('') // reset de l'erreur
 		return true
 	}
 
@@ -32,9 +48,9 @@ export default function InputAnswer ({
 		}
 	}
 	const handleClick = () => {
-		if (verifyInput(answer)) {
-			setHistoryInput([...historyInput, inputValue.toUpperCase()])
-			setInputValue('')
+		if (verifyInput(answer)) { // si l'input est correct
+			setHistoryInput([...historyInput, inputValue.toUpperCase()]) // ajout de l'input dans l'historique
+			setInputValue('') // reset de l'input
 		}
 	}
 	return (
@@ -43,6 +59,7 @@ export default function InputAnswer ({
 				placeholder="Entrez votre réponse" onKeyDown={handlePressEnter}></input>
 			<button onClick={handleClick}>Envoyer</button>
 			<p>{nbInputStringLeft}</p>
+			<p>{textErrorInput}</p>
 		</div>
 	)
 }
--- src/components/WordHistory.tsx
+++ src/components/WordHistory.tsx
@@ -7,16 +7,32 @@ export type WordHistoryProps = {
 }
 export default function WordHistory ({ answer, historyInput, inputValue }: WordHistoryProps) {
 	// state
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	const word_letter = answer.split('')
+
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	const verify_letter = (letter: string, index: number, word_letter: string[]) => {
+		if (letter === word_letter[index]) {
+			return 'green'
+		} else if (answer.includes(letter)) {
+			return 'orange'
+		} else {
+			return 'red'
+		}
+	}
 
 	// render
 	return (
 		<div>
 			<p>{inputValue}</p>
-			<ul>
-				{historyInput.map((answer) => (
-					<li key={answer}>{answer}</li>
-				))}
-			</ul>
+			{historyInput.map((answer, index) => (
+				<div key={index} className={'container-history'}>
+					{/* eslint-disable-next-line @typescript-eslint/naming-convention */}
+					{answer.split('').map((letter, index_letter) => (
+						<p key={index_letter} className={verify_letter(letter, index_letter, word_letter)}>{letter}</p>
+					))}
+				</div>
+			))}
 		</div>
 	)
 }

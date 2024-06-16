6e4bdb228f8d59adeddb00191e4705900224c8a0
BluedyAishela_ <94249947+BluedyRimuru@users.noreply.github.com>
2024-06-16 18:19:02 +0200

    Force Axios Https

--- src/hook/useApi.ts
+++ src/hook/useApi.ts
@@ -1,20 +1,25 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 
+axios.defaults.baseURL = 'https://api.beltaria.fr/api/'
+
 export default function useApi<T> (path: string) {
 	const [data, setData] = useState<T>()
 	const [error, setError] = useState<any>()
+
 	useEffect(() => {
 		const fetchData = async () => {
-			axios.get<T>('https://api.beltaria.fr/api/' + path)
-				.then(response => {
-					setData(response.data)
-				}).catch((error) => {
-					setError(error)
-				})
+			try {
+				const response = await axios.get<T>(path)
+				setData(response.data)
+			} catch (error) {
+				setError(error)
+			}
 		}
+
 		fetchData()
-	}, [])
+	}, [path]) // Ajoutez 'path' comme dépendance pour recharger lorsque 'path' change
+
 	return {
 		data,
 		error
